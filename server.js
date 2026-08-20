1032ee77a5090e3debf4883363cbbef4a552e859
scottdikkers <scott@dikkers.com>
2026-08-20 05:31:53 -0500

    Update server.js

--- server.js
+++ server.js
@@ -695,6 +695,14 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
 
+app.get('/support', (req, res) => {
+  res.sendFile(__dirname + '/public/support.html');
+});
+
+app.get('/privacy', (req, res) => {
+  res.sendFile(__dirname + '/public/privacy.html');
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Comedy Mastermind server running on port ${PORT}`);
